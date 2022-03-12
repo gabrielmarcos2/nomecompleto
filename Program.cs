8b9b19e0daf5d4b25de2b1b706d9f0a8a0285de4
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-03-11 22:38:55 -0300

    finalizado tarefa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string nome, sobrenome, sobrenomeEmMaiuscula;
+
+Console.Write("Digite seu  primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeEmMaiuscula = sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo:  {nome} {sobrenome}");
+
+Console.WriteLine($"Nmoe de catalogo: {sobrenomeEmMaiuscula}, {nome}");
